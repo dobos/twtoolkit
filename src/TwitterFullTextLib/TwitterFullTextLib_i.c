10f24914cfb8a0af84ffc94b9d38d81db71aed04
Laszlo Dobos <dobos@complex.elte.hu>
2013-10-10 17:52:51 +0200

    Full-text search filter build options fixed.

--- src/TwitterFullTextLib/TwitterFullTextLib_i.c
+++ src/TwitterFullTextLib/TwitterFullTextLib_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Wed Sep 18 21:07:06 2013
+/* at Thu Oct 10 17:51:43 2013
  */
 /* Compiler settings for TwitterFullTextLib.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
