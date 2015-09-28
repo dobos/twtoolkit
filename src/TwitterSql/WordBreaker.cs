290387fa54082ef2b950c0cff8cbcf93a7f0d3f2
Laszlo Dobos <dobos@complex.elte.hu>
2015-09-28 17:59:32 +0200

    Updated word breaker from dkondor.

--- src/TwitterSql/WordBreaker.cs
+++ src/TwitterSql/WordBreaker.cs
@@ -1,7 +1,5 @@
 using System;
-using System.IO;
 using System.Data;
-using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using System.Collections;
 using System.Collections.Generic;
@@ -19,8 +17,21 @@ namespace TwitterSql
             public short Pos;
         }
 
+
         [Microsoft.SqlServer.Server.SqlFunction(TableDefinition = "word nvarchar(140), id smallint, pos smallint", FillRowMethodName = "WordBreaker_Fill")]
         public static IEnumerable WordBreaker(SqlString text)
+        {
+            return WordBreaker3(text, 4, 2, false, 0);
+        }
+
+        [Microsoft.SqlServer.Server.SqlFunction(TableDefinition = "word nvarchar(140), id smallint, pos smallint", FillRowMethodName = "WordBreaker_Fill")]
+        public static IEnumerable WordBreaker2(SqlString text, SqlInt16 minlen, SqlInt16 repmax, SqlBoolean hashtaguser)
+        {
+            return WordBreaker3(text, minlen, repmax, hashtaguser, 0);
+        }
+
+        [Microsoft.SqlServer.Server.SqlFunction(TableDefinition = "word nvarchar(140), id smallint, pos smallint", FillRowMethodName = "WordBreaker_Fill")]
+        public static IEnumerable WordBreaker3(SqlString text, SqlInt16 minlen, SqlInt16 repmax, SqlBoolean hashtaguser, SqlInt16 maxlen)
         {
             var words = new List<IndexWord>();
 
@@ -28,7 +39,7 @@ namespace TwitterSql
             int count = _text.Length;
 
             const string whitespace = " \t\n\r";
-            const string punctuation = ",.!?;:\"-/+(){}[]\\`´_^~";
+            const string punctuation = ",.!?;:\"-/+(){}[]\\`?_^~<>";
 
             short word_idx = 0;     //word count
             int ws = 0;		        // word start
@@ -59,7 +70,7 @@ namespace TwitterSql
             else
             {
                 count = 0;
-                return words;
+                yield break;
             }
 
             // Read input letter by letter and put together into the word buffer
@@ -81,15 +92,16 @@ namespace TwitterSql
 
                 if (isWhitespace || isPunctuation || c == '\0')
                 {
-                    if (wc > 3 && !isnoisy && !isurl)
+                    if (wc >= (int)minlen && !isnoisy && !isurl)
                     {
-                        //pWordSink->PutWord(wc, source + ws, wc, ws);
-                        //pWordSink->PutBreak(WORDREP_BREAK_EOW);
-                        IndexWord tmp = new IndexWord();
-                        tmp.Word = _text.Substring(ws, wc);
-                        tmp.Pos = (short)ws;
-                        tmp.ID = word_idx++;
-                        words.Add(tmp);
+                        if (maxlen == 0 || wc <= (int)maxlen)
+                        {
+                            IndexWord tmp = new IndexWord();
+                            tmp.Word = _text.Substring(ws, wc);
+                            tmp.Pos = (short)ws;
+                            tmp.ID = word_idx++;
+                            yield return tmp;
+                        }
                     }
 
                     wc = 0;
@@ -124,12 +136,14 @@ namespace TwitterSql
                         lc = 1;
                     }
 
-                    isnoisy |= lc >= 3;							// letter repeated more than trice
+                    isnoisy |= lc > (int)repmax;							// letter repeated more than trice
 
+                    if (wc == 0 && hashtaguser) if (c == '#' || c == '@') goto nosymbols;
                     isnoisy |= (bl > 0x05);						// Not latin greek or cyrillic
                     isnoisy |= (bl == 0x00 && bu < 0x41);					// Symbols
                     isnoisy |= (bl == 0x00 && 0x7B <= bu && bu < 0xC0);		// More symbols
                     isnoisy |= (bl == 0x03 && bu < 0x80);		// More symbols
+                nosymbols:
 
                     // append to the word
                     if (wc == 0)
@@ -140,9 +154,7 @@ namespace TwitterSql
                 }
             }
 
-
-
-            return words;
+            yield break;
         }
 
         public static void WordBreaker_Fill(object item, out SqlString word, out SqlInt16 id, out SqlInt16 pos)
@@ -156,3 +168,4 @@ namespace TwitterSql
     }
 
 }
+
